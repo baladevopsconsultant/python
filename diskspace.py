6d26a7d9a76769c11814c28f7d996dbd189e3978
bala <bala@python.com>
2019-05-28 19:38:17 +0530

    diskspace code

--- diskspace.py
+++ diskspace.py
@@ -1,8 +1,8 @@
 #! /bin/python
 import subprocess
-threshold=0
+threshold=80
 child=subprocess.Popen(['df','-h'],stdout=subprocess.PIPE)
 output = child.communicate()[0].strip().split("\n")
 for x in output[1:]:
-    if int(x.split()[-2][:-1]) == 0:
+    if int(x.split()[-2][:-1]) == 80:
        print x
