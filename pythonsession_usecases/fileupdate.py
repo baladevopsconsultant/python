200973abcf9c2293fb19301c61dfc9a4e7228a4f
root <root@python.us-central1-a.c.astute-solstice-258207.internal>
2020-09-22 19:24:40 +0000

    Python first usecase - append the file with specific content

---
+++ pythonsession_usecases/fileupdate.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python2.7
+def fileadd(*argv):
+    for i in range(*argv):
+        a=str(i)
+        Lines=["<record>\n","<refid>ref-" + a +"<</refid>\n","<url>http://0.0.0.11/test-conv/res.1</url>\n","</record>\n"]
+        file1 = open("urlfile.txt","a+")
+        file1.writelines(Lines)
+        file1.close()
+if __name__ == "__main__":
+    import sys
+    value=sys.argv[1]
+    value=int(value)
+    fileadd(1,value)
