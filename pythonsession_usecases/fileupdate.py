b925d54d777767d966b60f1e07f7714f74854fef
python <python@practice.com>
2020-09-26 16:33:50 +0000

    dynamic url update from the urlfile - v1

--- pythonsession_usecases/fileupdate.py
+++ pythonsession_usecases/fileupdate.py
@@ -4,14 +4,12 @@ def fileadd(*argv):
     urllist=[]
     for line in url:
         striping = line.strip()
-        print (striping)
         urllist.append(striping)
     url.close()
     try:
         for i in range(*argv):
             a=str(i)
             b=i-1
-            print (b)
             link=urllist[b]
             Lines=["<record>\n","<refid>ref-" + a +"<</refid>\n","<url>" + link + "</url>\n","</record>\n"]
             file1 = open("urlfile.txt","a+")
