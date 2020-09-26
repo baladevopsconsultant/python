638ac460409493ac7353d2675c6e1ae5c95b2803
python <python@practice.com>
2020-09-26 16:27:00 +0000

    dynamic url update from the urlfile

--- pythonsession_usecases/fileupdate.py
+++ pythonsession_usecases/fileupdate.py
@@ -1,11 +1,24 @@
 #! /usr/bin/python2.7
 def fileadd(*argv):
-    for i in range(*argv):
-        a=str(i)
-        Lines=["<record>\n","<refid>ref-" + a +"<</refid>\n","<url>http://0.0.0.11/test-conv/res.1</url>\n","</record>\n"]
-        file1 = open("urlfile.txt","a+")
-        file1.writelines(Lines)
-        file1.close()
+    url = open("urlfile","r")
+    urllist=[]
+    for line in url:
+        striping = line.strip()
+        print (striping)
+        urllist.append(striping)
+    url.close()
+    try:
+        for i in range(*argv):
+            a=str(i)
+            b=i-1
+            print (b)
+            link=urllist[b]
+            Lines=["<record>\n","<refid>ref-" + a +"<</refid>\n","<url>" + link + "</url>\n","</record>\n"]
+            file1 = open("urlfile.txt","a+")
+            file1.writelines(Lines)
+            file1.close()
+    except IndexError as error:
+        print ("URL not matching the Range of Refid")
 if __name__ == "__main__":
     import sys
     value=sys.argv[1]
